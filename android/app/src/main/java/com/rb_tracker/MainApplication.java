79ebf28b2b617b6fb4b0723286fe12fb86c9b5a2
Bob <catalin.bora@gmail.com>
2019-09-23 14:12:58 +0300

    Android Firebase crash bugfix.

--- android/app/src/main/java/com/rb_tracker/MainApplication.java
+++ android/app/src/main/java/com/rb_tracker/MainApplication.java
@@ -15,6 +15,8 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.List;
 
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -29,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseFirestorePackage());
       return packages;
     }
 
